a22b60ea5f614316d431a2a91873c59a9e843c32
Jonathan Poulter <poulter7@gmail.com>
2011-08-06 03:25:05 +0100

    number of completed questions

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,8 @@ class Answer < ActiveRecord::Base
   belongs_to :participant
   belongs_to :section
   serialize  :answer_list
+
+  def answered_count
+    answer_list.count{|a|!a.nil?}
+  end
 end
--- test/unit/answer_test.rb
+++ test/unit/answer_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup do
+    @a_one = answers(:a_one)
+  end
+
+  test "count of answered questions" do
+    
+    @a_one.answer_list = ['abc',nil,nil]
+    c = @a_one.answered_count
+    assert c == 1, c.to_s
   end
 end
